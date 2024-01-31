1166ef34d288c3bb6a0693fd85c345957ad31f97
paul172v <paul172v@aol.com>
2024-01-31 02:48:47 +0000

    added a modal via library

--- src/sections/portfolio/Portfolio.tsx
+++ src/sections/portfolio/Portfolio.tsx
@@ -1,71 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import { Element } from "react-scroll";
+import ReactModal from "react-modal";
 
 import classes from "./Portfolio.module.scss";
 
 const Portfolio: React.FC = () => {
+  const [isModalActive, setIsModalActive] = useState(false);
+  const [selectedImageName, setSelectedImageName] = useState("");
+
+  //// Set HTMLroot location of modal
+  ReactModal.setAppElement("#modal");
+
+  //// Functions
+
+  const openModalHandler = (img: string) => {
+    setSelectedImageName(img);
+    setIsModalActive(true);
+    console.log(`0${Number(img)}`);
+  };
+
+  const closeModalHandler = () => {
+    setIsModalActive(false);
+  };
+
+  const prevModalHandler = () => {
+    const subtractedNumber = Number(selectedImageName) - 1;
+
+    if (selectedImageName === "01") {
+      setSelectedImageName("09");
+    } else {
+      setSelectedImageName(`0${subtractedNumber}`);
+    }
+  };
+
+  const nextModalHandler = () => {
+    const increasedNumber = Number(selectedImageName) + 1;
+
+    if (selectedImageName === "09") {
+      setSelectedImageName("01");
+    } else {
+      setSelectedImageName(`0${increasedNumber}`);
+    }
+  };
+
+  const imagesArr = ["01", "02", "03", "04", "05", "06", "07", "08", "09"];
+
   return (
-    <section className={classes["section-portfolio"]}>
-      <Element name="anchor-portfolio" className="element">
-        <h2 className={classes["sub-heading"]}>Portfolio</h2>
-      </Element>
-      <div className={classes["gallery-wrapper"]}>
-        <img
-          src="./01.jpg"
-          alt="photo"
-          className={classes.photo}
-          id={classes["01"]}
-        />
-        <img
-          src="./02.jpg"
-          alt="photo"
-          className={classes.photo}
-          id={classes["02"]}
-        />
-        <img
-          src="./03.jpg"
-          alt="photo"
-          className={classes.photo}
-          id={classes["03"]}
-        />
-        <img
-          src="./04.jpg"
-          alt="photo"
-          className={classes.photo}
-          id={classes["04"]}
-        />
-        <img
-          src="./05.jpg"
-          alt="photo"
-          className={classes.photo}
-          id={classes["05"]}
-        />
-        <img
-          src="./06.jpg"
-          alt="photo"
-          className={classes.photo}
-          id={classes["06"]}
-        />
-        <img
-          src="./07.jpg"
-          alt="photo"
-          className={classes.photo}
-          id={classes["07"]}
-        />
-        <img
-          src="./08.jpg"
-          alt="photo"
-          className={classes.photo}
-          id={classes["08"]}
-        />
-        <img
-          src="./09.jpg"
-          alt="photo"
-          className={classes.photo}
-          id={classes["09"]}
-        />
-      </div>
-    </section>
+    <React.Fragment>
+      <section className={classes["section-portfolio"]}>
+        <Element name="anchor-portfolio" className="element">
+          <h2 className={classes["sub-heading"]}>Portfolio</h2>
+        </Element>
+        <div className={classes["gallery-wrapper"]}>
+          {imagesArr.map((img) => (
+            <img
+              key={img}
+              src={`./${img}.jpg`}
+              alt="photo"
+              className={classes.photo}
+              onClick={() => openModalHandler(`${img}`)}
+            />
+          ))}
+        </div>
+      </section>
+      <ReactModal
+        className={classes.modal}
+        isOpen={isModalActive}
+        onRequestClose={closeModalHandler}
+      >
+        <img src={`./${selectedImageName}.jpg`} alt="Selected" />
+        <div className={classes["u-row"]}>
+          <button onClick={prevModalHandler}>Prev</button>
+          <button onClick={closeModalHandler}>Close</button>
+          <button onClick={nextModalHandler}>Next</button>
+        </div>
+      </ReactModal>
+    </React.Fragment>
   );
 };
 
